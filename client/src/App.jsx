6148fe31537bb831d92df89c30861b6ec7ea476a
red-arrow-005 <karthik.a2219905@gmail.com>
2024-06-05 18:29:27 +0530

    "Added Auth component and conditional rendering to App.jsx; updated component exports in index.js"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
-import { ChannelListContainer, ChannelContainer } from './components';
-import './App.css'
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
+import './App.css';
 const apiKey = '7sad452pgpea';
 
-const client = StreamChat.getInstance(apiKey)
+const client = StreamChat.getInstance(apiKey);
+
+const authToken = false;
 
 const App = () => {
-    return (
-        <div className='app__wrapper'>
-            <Chat client={client} theme='team light'>
-                <ChannelListContainer />
-                <ChannelContainer />
-            </Chat>
-        </div>
-    )
-}
+  if (!authToken) return <Auth />;
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
+    </div>
+  );
+};
 
-export default App
+export default App;
