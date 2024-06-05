6fe18b56ecafff1f18af54019099492aaad9b98d
red-arrow-005 <karthik.a2219905@gmail.com>
2024-06-05 15:56:15 +0530

    Added axios dependency, modified App.jsx to include Auth component and conditional rendering, and reformatted code in ChannelContainer.jsx and ChannelListContainer.jsx with added semicolons and removed trailing whitespace.

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const ChannelContainer = () => {
-    return (
-        <div>ChannelContainer</div>
-    )
-}
+  return <div>ChannelContainer</div>;
+};
 
-export default ChannelContainer
+export default ChannelContainer;
