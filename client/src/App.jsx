8ec0bb589df11ff3207cdb8f9c862752555864d1
red-arrow-005 <karthik.a2219905@gmail.com>
2024-06-14 13:16:52 +0530

    "Update App.jsx with new API key and refactor EditChannel component with new functionality"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 
 const cookies = new Cookies();
 
-const apiKey = '7sad452pgpea';
+const apiKey = 'd64mtm5r2rmp';
 const authToken = cookies.get('token');
 
 const client = StreamChat.getInstance(apiKey);
